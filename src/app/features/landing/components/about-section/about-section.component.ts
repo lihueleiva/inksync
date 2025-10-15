47e612153937add4e9f385d50991046d460d87b9
Lihue Leiva <leivalihue@gmail.com>
2025-10-15 15:48:38 -0300

    feat(landing): create about artist section

---
+++ src/app/features/landing/components/about-section/about-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ink-about-section',
+  imports: [],
+  templateUrl: './about-section.component.html',
+  styleUrl: './about-section.component.scss'
+})
+export class AboutSectionComponent {
+
+}
--- src/app/features/landing/pages/home/home.component.ts
+++ src/app/features/landing/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from '../../components/hero-section/hero-section.component';
 import { GallerySectionComponent } from '../../components/gallery-section/gallery-section.component';
-
+import { AboutSectionComponent } from '../../components/about-section/about-section.component';
 @Component({
   selector: 'ink-home',
   standalone: true,
-  imports: [HeroSectionComponent, GallerySectionComponent],
+  imports: [HeroSectionComponent, GallerySectionComponent, AboutSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
