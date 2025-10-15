357eace5bae9a9e921458a8167dfce08cf1aa713
Lihue Leiva <leivalihue@gmail.com>
2025-10-15 16:04:42 -0300

    feat(landing): create process and care section

---
+++ src/app/features/landing/components/process-care-section/process-care-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@Component({
+  selector: 'ink-process-care-section',
+  imports: [MatIconModule],
+  templateUrl: './process-care-section.component.html',
+  styleUrl: './process-care-section.component.scss'
+})
+export class ProcessCareSectionComponent {
+
+}
--- src/app/features/landing/pages/home/home.component.ts
+++ src/app/features/landing/pages/home/home.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { HeroSectionComponent } from '../../components/hero-section/hero-section.component';
 import { GallerySectionComponent } from '../../components/gallery-section/gallery-section.component';
 import { AboutSectionComponent } from '../../components/about-section/about-section.component';
+import { ProcessCareSectionComponent } from '../../components/process-care-section/process-care-section.component';
+
+
 @Component({
   selector: 'ink-home',
   standalone: true,
-  imports: [HeroSectionComponent, GallerySectionComponent, AboutSectionComponent],
+  imports: [HeroSectionComponent, GallerySectionComponent, AboutSectionComponent, ProcessCareSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
